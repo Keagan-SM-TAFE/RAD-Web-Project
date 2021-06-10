bbb64ff6ddcdc98156e0980a314a58d199dd519b
Keagan-SM-TAFE <79202414+Keagan-SM-TAFE@users.noreply.github.com>
2021-06-10 17:20:22 +0800

    Changes to TEST

---
+++ TEST/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<title>!!!TEST!!!</title>
+<script src="js/ajax_subscribe.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<div class="content"> 
+	<div class="container-fluid">
+		<h2>!!!TEST!!!</h2>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="main-content">
+					<div class="susbcribe-container">
+						<div class="top">
+							<h2>Subscribe Now</h2>													
+						</div>
+						<div class="bottom">							
+							<div class="status"></div>						
+							<form action="#" id="subscribeForm" method="post">							
+								<input type="text" class="form-control" id="name" placeholder="Full Name" required="">	
+								<span class="text-danger hidden" id="nameError"></span>
+								<input type="email" class="form-control" id="email" placeholder="E-mail" required="">
+								<span class="text-danger hidden" id="emailError"></span>
+								<input type="button" id="subscribeButton" value="Subscribe Now">
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	 
+	</div>       
+</div>   		
+</body>
+</html>
