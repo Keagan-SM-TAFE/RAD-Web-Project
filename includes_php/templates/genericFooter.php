65cb7cbfb64cb1e0084fbeac3c93ee442bef1c4f
Keagan-SM-TAFE <79202414+Keagan-SM-TAFE@users.noreply.github.com>
2021-06-11 11:40:59 +0800

    Delete includes_php directory

--- includes_php/templates/genericFooter.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer -->
-<footer class="bg-light text-center">
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                <span class="text-dark"><p>Copyright &copy; 2020 - <?php print date('Y');?> 
-                RAD Group Project</p></span>
-        </div>
-    </footer>
-</body>
-</html>
