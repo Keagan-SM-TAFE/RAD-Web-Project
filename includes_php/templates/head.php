869477a6cc61c58969a0c046521f71c570fc9f19
KeaganYoung <keagan554@hotmail.com>
2021-06-16 13:32:33 +0800

    Update head.php
    included script tag

--- includes_php/templates/head.php
+++ includes_php/templates/head.php
@@ -15,11 +15,11 @@
 <meta name="author" content="Keagan Young">
 <meta name="author" content="Andrew Tuitupou">
 <meta name="keywords" content="Rapid Application Development Project">
-<link rel="icon" type="image/png" sizes="16x16" 
+<link rel="icon" type="image/png" sizes="16x16"
 href="images/favicon/favicon-16x16.png">
 <!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" 
-rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" >
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+<link href="privateAdmin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<script src="privateAdmin/vendor/jquery/jquery.min.js"></script>
 </head>
