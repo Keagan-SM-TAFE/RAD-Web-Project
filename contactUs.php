32f3a439ca645e1e6121e7baf20be13595a7a581
KeaganYoung <keagan554@hotmail.com>
2021-06-16 15:19:04 +0800

    Update contactUs.php

--- contactUs.php
+++ contactUs.php
@@ -1,3 +1,43 @@
+<?php
+$ERROR = "";
+use PHPMailer\PHPMailer\PHPMailer;
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+    $name = $_POST['contactName'];
+    $email = $_POST['email'];
+    $subject = $_POST['contactSubject'];
+    $contactMessage = $_POST['contactMessage'];
+    require_once "PHP_Mailer/PHPMailer.php";
+    require_once "PHP_Mailer/SMTP.php";
+    require_once "PHP_Mailer/Exception.php";
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "thetruth2451@gmail.com";
+    $mail->Password = "";
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+    $mail->isHTML(true);
+    $mail->setFrom("Atuitpou2@gmail.com",$name);
+    $mail->addAddress("thetruth2451@gmail.com");
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $contactMessage;
+
+    if($mail->send())
+    {
+        $status = "success";
+        $response = "Email is sent!";
+        header("Location: index.php");
+    }    
+    else
+    {
+        $status = "failed";
+        $response = "Something went wrong: <br>". $mail->ErrorInfo;
+    }
+    //exit(json_encode(array("status" => $status, "response" => $response)));
+}
+?>
 <!-- START Head -->
 <?php require_once "includes_php\\templates\\head.php"; ?>
 <script src="main.js"></script>
@@ -17,7 +57,7 @@ if($messageSent):
 else:
 ?>
 <div class="container">
-    <form action="includes_php\phpActions\sendMessage.php" method="POST" class="form">
+    <form action="" method="POST" class="form">
         <div class="mb-3">
             <label for="contactName" class="form-label">Your Name</label>
             <input type="text" class="form-control form-control-lg" id="contactName" 
