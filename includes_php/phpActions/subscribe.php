f5ebe1061f6cc41df3df83c7706e59996514a8c1
KeaganYoung <keagan554@hotmail.com>
2021-06-16 13:31:15 +0800

    Update subscribe.php
    added newsflash and newsletter functionality

--- includes_php/phpActions/subscribe.php
+++ includes_php/phpActions/subscribe.php
@@ -5,12 +5,26 @@ $conn = openConn();
 
 $userDate = date("Y-m-d H:i:s");
 require_once "sanitiseNewsletterInput.php";
-$sqlQuery = "INSERT INTO `moviedatabase_subscribers`(`name`, `email`, `dateCreated`, `is_Subscribed`) 
-VALUES ('$verifiedName', '$verifiedEmail', '$userDate', '1');";
+$news = 0;
+$flash = 0;
+$all = 0;
+if(isset($_POST['news'])){
+  $news = 1;
+}
+if(isset($_POST['flash'])){
+  $flash = 1;
+}
+if(isset($_POST['news']) && isset($_POST['flash'])){
+  $all = 1;
+}
+$sqlQuery = "INSERT INTO `moviedatabase_subscribers`(`name`, `email`, `dateCreated`, `is_Subscribed`, `newsletter`, `newsflash`)
+VALUES ('$verifiedName', '$verifiedEmail', '$userDate', $all, $news, $flash);";
 
 if (count($_POST)>0) {
     $stmt = $conn->prepare($sqlQuery);
     $stmt->execute();
 }
 closeConn($conn);
+header("Location: ../../index.php?msg=success");
+exit;
 ?>
