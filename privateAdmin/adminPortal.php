b1fca6d79396b0d58aec7eeacc8579477fa1ce79
KeaganYoung <keagan554@hotmail.com>
2021-06-16 13:45:21 +0800

    Re-Designed Admin Portal
    admin can change subscription preferences within the admin portal

--- privateAdmin/adminPortal.php
+++ privateAdmin/adminPortal.php
@@ -12,79 +12,27 @@
 <!-- All the important stuff for the Jummbotron/Showcase -->
 <?php require_once "..\\includes_php\\templates\\showcase.php"; ?>
 
-<!-- Connection for display suscribers -->
-<?php require_once "..\\includes_php\\databaseConnection\\databaseConnection.php"; ?>
-
 <!-- Start of the main content -->
 <section class="container-fluid">
+<?php
 
+require_once "..\\includes_php\\databaseConnection\\databaseConnection.php";
+	$unsuscribe= null;
+?>
     <div class="container col-lg-8">
     <h2>Administrator PortHole</h2>
     <br>
     </div>
  	<div class="container col-lg-8">
  		<form method="post" class = "col-lg-4" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-             <div class="mb-3">
-                <label for="Unsuscribe" class="form-label">Unsuscribe:</label>
-                <input type="text" class="form-control form-control-lg" id="email" placeholder="Enter email"  name="email" value="">
-            </div>
-                <button type="submit" class="btn btn-outline-success btn-lg">Unsuscribe</button>
-                <br><br>
-        </form>
+                    <div class="mb-3">
+                        <label for="Unsuscribe" class="form-label">Unsuscribe:</label>
+                        <input type="text" class="form-control form-control-lg" id="unsuscribe" placeholder="Enter email"  name="unsuscribe" value="<?php echo $unsuscribe; ?>">
+                    </div>
+                    <button type="submit" class="btn btn-outline-success btn-lg">Unsuscirbe</button>
+                    <br><br>
+                </form>
  	</div>
-    
-<!-- Connection for display suscribers -->
-<?php require_once "..\\includes_php\\databaseConnection\\databaseConnection.php";?>
-
-<?php
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $conn = openConn();
-    $email = $_POST['email'];
-    $query = "UPDATE moviedatabase_subscribers SET is_Subscribed = 0 WHERE email= '$email';";
-    $conn->query($query);
-    echo    '
-            <div class="alert alert-success">
-                <strong>Success!</strong> Form Submitted!
-            </div>
-            ';
-    $query = "SELECT * FROM moviedatabase_subscribers WHERE is_Subscribed = 1;";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $result = $stmt->get_result();                            
-    if (isset($result->num_rows) && $result->num_rows > 0) {   
-        //Creation and headings Of the Table
-        echo    '
-                <div class="container">
-                <div class="table-responsive">
-                <table class="table table-hover table-condensed">
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Date Created</th>
-                    </tr>
-                ';
-        // output the data of each row
-        while ($row = $result->fetch_assoc()) {            
-            echo    "
-                    <tr>
-                        <td>" . $row["id"] . "</td>
-                        <td>" . $row["name"] . "</td>
-                        <td>" . $row["email"] . "</td>
-                        <td>" . $row["dateCreated"] . "</td>
-                    </tr>
-                    ";
-        }
-        echo    "
-                </table>
-                </div>
-                </div>
-                ";
-    }
-    closeConn($conn);
-}
-?>        
 </section>
 <br>
 <!-- All the important stuff for the <footer> -->
---
+++ privateAdmin/adminUnsubscribe.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- All the important stuff for the <head> -->
+<?php require_once "includes_php\\head.php"; ?>
+</head>
+<body>
+<!-- All the important stuff for the navbar -->
+<?php require_once "includes_php\\navBar.php"; ?>
+
+<!-- All the important stuff for the Jummbotron/Showcase -->
+<?php require_once "includes_php\\jumbotron.php"; ?>
+
+<!-- Start of the main content -->
+<section class="container-fluid">
+<?php
+
+require_once "includes_php\\databaseConnection\\databaseConnection.php";
+	$username = $password = $confirm= null;
+?>
+ 	<div class="container">
+ 		<h2>Administrator Sign Up</h2>
+ 		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <div class="mb-3">
+                <label for="username" class="form-label">Username:</label>
+                    <input type="text" class="form-control form-control-lg" id="username" 
+                        placeholder="Enter Username"  name="username" value="<?php echo $username; ?>">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                    <input type="text" class="form-control form-control-lg" id="genre" 
+                        placeholder="Enter Password"  name="password" value="<?php echo $password; ?>">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Confirm Password</label>
+                    <input type="text" class="form-control form-control-lg" id="genre" 
+                        placeholder="Confirm Password"  name="password" value="<?php echo $password; ?>">
+            </div>
+            <button type="submit" class="btn btn-outline-success btn-lg">Register</button>
+            <br><br>
+        </form>
+ 	</div>
+</section>
+<!-- All the important stuff for the <footer> -->
+    <footer class="bg-light text-center">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        <?php require_once "includes_php\\templates\\genericFooter.php"; ?>
+        </div>
+    </footer>
+</body>
+</html>
