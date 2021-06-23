b5def30e4a4f0b67ac3191b47bd54df081a1f717
thetruth2451 <80291700+thetruth2451@users.noreply.github.com>
2021-06-23 11:57:19 +0800

    CHANGES
    -More realistic subscriber names.

--- databaseSqlFiles/moviedatabase.sql
+++ databaseSqlFiles/moviedatabase.sql
@@ -2429,13 +2429,13 @@ CREATE TABLE `moviedatabase_subscribers` (
 --
 
 INSERT INTO `moviedatabase_subscribers` (`id`, `name`, `email`, `dateCreated`, `is_subscribed`, `newsletter`, `newsflash`) VALUES
-(1, 'test', 'test@email.com', '2021-06-14 09:32:53', 1, 1, 1),
-(2, 'test1', 'test1@email.com', '2021-06-14 09:33:18', 1, 1, 1),
-(3, 'test2', 'test2@yahoo.com', '2021-06-14 09:33:38', 1, 1, 1),
-(4, 'xxz', 'zxz@dd', '2021-06-15 08:10:31', 1, 1, 1),
-(5, 'xxz', 'zxz@dds', '2021-06-15 08:13:55', 1, 1, 1),
-(6, 'ds', 'zxz@dds', '2021-06-15 08:14:17', 1, 1, 1),
-(7, 'daa', 'asazxc@email.com', '2021-06-15 13:35:56', 0, 1, 0);
+(1, 'John', 'JohnF@gmail.com', '2021-06-14 09:32:53', 1, 1, 1),
+(2, 'Andrew', 'Andrew@hotmail.com', '2021-06-14 09:33:18', 1, 1, 1),
+(3, 'Sam', 'SamBus@yahoo.com', '2021-06-14 09:33:38', 1, 1, 1),
+(4, 'Frank', 'Frank@gmail.com', '2021-06-15 08:10:31', 1, 1, 1),
+(5, 'Tom', 'Tom@yahoo.com', '2021-06-15 08:13:55', 1, 1, 1),
+(6, 'Timothy', 'Tim@gmail.com', '2021-06-15 08:14:17', 1, 1, 1),
+(7, 'Sarah', 'Harper@gmail.com', '2021-06-15 13:35:56', 0, 1, 0);
 
 --
 -- Indexes for dumped tables
