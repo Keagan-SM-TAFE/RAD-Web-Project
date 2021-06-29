6cee021bc03f76d8b4c05ce643be7a2b07691ba9
Keagan-SM-TAFE <79202414+Keagan-SM-TAFE@users.noreply.github.com>
2021-06-29 12:26:22 +0800

    genericFooter.php adheres to WCAG

--- includes_php/templates/genericFooter.php
+++ includes_php/templates/genericFooter.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="bg-light text-center">
+<footer class="bg-light text-center" aria-label="Generic footer">
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
                 <span class="text-dark"><p>Copyright &copy; 2020 - <?php print date('Y');?> 
                 RAD Group Project</p></span>
