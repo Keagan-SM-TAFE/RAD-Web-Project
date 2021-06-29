5dcaf3242892bb65da58110ea69a5f0cb16f4c6d
Keagan-SM-TAFE <79202414+Keagan-SM-TAFE@users.noreply.github.com>
2021-06-29 12:12:39 +0800

    subscribtionFooter.php adheres to WCAG

--- includes_php/templates/subscribtionFooter.php
+++ includes_php/templates/subscribtionFooter.php
@@ -1,58 +1,42 @@
 <!-- Footer -->
 <footer class="bg-light text-center">
-        <div class="container p-4 pb-0">
-            <section>
-            <?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'success'){  echo "<div class='alert alert-success'>You successfully subscribed to our newsletter.</div>"; }?>
-
-            <form action="includes_php\phpActions\subscribe.php" method="POST">
-                <div class="row d-flex justify-content-center">
+    <div class="container p-4 pb-0">
+        <section>
+<?php if(isset($_REQUEST['msg']) && $_REQUEST['msg'] == 'success')
+{  echo "<div class='alert alert-success'>You successfully subscribed to our newsletter.</div>"; }?>
+            <form class="row gy-2 gx-3 align-items-center"
+            action="includes_php\phpActions\subscribe.php" method="POST">
                 <div class="col-auto">
                     <p class="pt-2">
                     	<br>
                         <strong>Subscribe to our newsletter</strong>
                     </p>
                 </div>
-                <div class="col-md-7 col-12">
-                    <div class="row g-3">
-                        <div class="col-md">
-                        	Name
-                            <div class="form-floating">
-
-                                <input type="text" class="form-control" id="floatingInputGrid"
-                                name="newsletterName" value="">
-
-                                <label for="floatingInputGrid">Name</label>
-
-                            </div>
-                        </div>
-                        <div class="col-md">
-                        	Email address
-                            <div class="form-floating">
-                                <input type="email" class="form-control" id="floatingInputGrid"
-                                name="newsletterEmail" value="">
-                                <label for="floatingInputGrid">Email address</label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                        	<br>
-                            <label style="float:left;"><input type="checkbox" name="news" checked>Newsletter</label> <br>
-                            <label style="float:left;"><input type="checkbox" name="flash" checked>Newsflash</label>
-                        </div>
-                    </div>
+                <div class="col-auto">
+                    <label class="visually-hidden" for="autoSizingInput" aria-label="Enter your name">Enter your name</label>
+                    <input type="text" tabindex="1" class="form-control" id="autoSizingInput" placeholder="Enter your name">
                 </div>
                 <div class="col-auto">
-                	<br>
-                    <button type="submit" name="submit" class="btn btn-primary mb-4">Subscribe</button>
+                    <label class="visually-hidden" for="email" aria-label="Enter your email">Enter your email</label>
+                    <input type="email" tabindex="2" class="form-control" id="email" placeholder="Enter your email">
                 </div>
+                <div class="col-md">
+                    <br>
+                    <label style="float:left;"><input type="checkbox" name="news" checked>Newsletter</label> <br>
+                    <label style="float:left;"><input type="checkbox" name="flash" checked>Newsflash</label>
+                </div>
+                <div class="col-auto">
+                    <button type="submit" name="submit" aria-label="Subscribe to our newsletter button"
+                    class="btn btn-primary mb-4" id="SubscribeButton">Subscribe</button>
                 </div>
             </form>
-            </section>
-        </div>
-        <br>
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                <span class="text-dark"><p>Copyright &copy; 2020 - <?php print date('Y');?>
-                RAD Group Project</p></span>
-        </div>
-    </footer>
+        </section>
+    </div>  
+    <br>    
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        <span class="text-dark"><p>Copyright &copy; 2020 - <?php print date('Y');?>
+        RAD Group Project</p></span>
+    </div>
+</footer>
 </body>
 </html>
